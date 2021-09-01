6ea8115c086d26097d024582b9fad25c1b9f99a0
Osc-53215 <80727193+Osc-53215@users.noreply.github.com>
2021-08-31 20:34:16 -0500

    styled the text under the pictures

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -25,13 +25,12 @@ const NavOptions = () => {
             keyExtractor = {(item) => item.id}
             renderItem = {( {item} ) => (
                 <TouchableOpacity style = {tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
-                    
                     <View>
                         <Image
                             style = {{ width: 120, height: 120, resizeMode: 'contain' }} 
                             source = {{ uri: item.image }}
                         />
-                        <Text>{item.title}</Text>
+                        <Text style = {tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
                     </View>
                 </TouchableOpacity>
             )}
